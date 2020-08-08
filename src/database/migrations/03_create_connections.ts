083a93dd32dc1fbad907dc931927968ec00c7bf5
Kelvin Maues <kgmdeveloper@gmail.com>
2020-08-08 13:00:15 -0300

    Fix error to create connection on database

--- src/database/migrations/03_create_connections.ts
+++ src/database/migrations/03_create_connections.ts
@@ -12,7 +12,7 @@ export async function up(knex: Knex) {
       .onUpdate("CASCADE")
       .onDelete("CASCADE");
 
-    table.timestamp("create_at").defaultTo("now()").notNullable();
+    table.timestamp("create_at").defaultTo("CURRENT_TIMESTAMP").notNullable();
   });
 }
 
