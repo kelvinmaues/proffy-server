3c4a311f0c120a4dea869cd374f58bd408769b6b
Kelvin Maues <kgmdeveloper@gmail.com>
2020-08-08 12:55:25 -0300

    Create connection feature with new route and controller

---
+++ src/controllers/ConnectionsController.ts
@@ -0,0 +1,14 @@
+import { Request, Response, request, response } from "express";
+import db from "../database/connection";
+
+export default class ConnectionsController {
+  async create(req: Request, resp: Response) {
+    const { user_id } = req.body;
+
+    await db("connections").insert({ user_id });
+
+    return resp.status(201).send();
+  }
+
+  async index(req: Request, resp: Response) {}
+}
